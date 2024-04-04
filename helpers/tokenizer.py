6a25246442a11450d81be57dd462c9e084022bab
gurudewan <gauravdewan@live.com>
2024-04-04 15:24:49 +0100

    update consts

--- helpers/tokenizer.py
+++ helpers/tokenizer.py
@@ -1,17 +1,15 @@
 import tiktoken
-from consts import Consts
-
-consts = Consts()
+from consts import consts
 
 
-def count_tokens(text, model=consts.OPENAI_MODEL):
+def count_tokens(text, model=consts().OPENAI_MODEL):
     encoding = tiktoken.encoding_for_model(model)
     tokens = encoding.encode(text)
     token_count = len(tokens)
     return token_count
 
 
-def tokenize_and_count(text, model=consts.OPENAI_MODEL):
+def tokenize_and_count(text, model=consts().OPENAI_MODEL):
     encoding = tiktoken.encoding_for_model(model)
     tokens = encoding.encode(text)
     token_count = len(tokens)
